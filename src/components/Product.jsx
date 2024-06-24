7ff95bdfacfb5f5fac85d4e5d29431bc9826beff
Erdem Atila <erdematila07@hotmail.com>
2024-06-24 21:24:44 +0300

    foundations of product and cart in css

---
+++ src/components/Product.jsx
@@ -0,0 +1,18 @@
+
+function Product() {
+
+  return (
+    <>
+    <div className="product">
+        <img src="https://cdn.akakce.com/z/apple/iphone-15.jpg" alt="" />
+        <div>
+            <p>IPhone 15</p>
+            <p>549</p>
+        </div>
+        <button>Add to cart</button>
+    </div>
+    </>
+  )
+}
+
+export default Product;
